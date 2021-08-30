84d610bb56a75ac7ad55201566a167ccf728c595
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-08-30 17:01:13 -0600

    Add dropdown menu for the program

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { program } from '@babel/types';
+import React, { useState } from 'react';
 import emailIcon from '../../assets/email.svg';
 import githubIcon from '../../assets/github-logo.png';
 import slackIcon from '../../assets/slack-logo.png';
@@ -7,12 +8,32 @@ import './UserProfile.css';
 
 
 const UserProfile = ({ user }) => {
+  const [isEditingProfile, setEditingProfile] = useState(false);
+  const [program, setProgram] = useState('Back-end Program')
+
+  const editProfile = () => {
+    if (!isEditingProfile) {
+      setEditingProfile(true)
+    } else {
+      setEditingProfile(false)
+    }
+  }
+
+  const handleChange = (event) => {
+    setProgram(event.target.value)
+  }
 
   return (
       <section className="profile-container">
         <img src={profilePic} className="profile-pic" alt="imported from Github profile"/>
         <h2 className="s-h2">{user.name}</h2>
-        <p className="s-text-body">Front-end Engineer</p>
+        <div>
+          {!!isEditingProfile ? 
+          <select className="drop-down s-content s-text-body" name="program" id="program" value={program} onChange={handleChange}>
+          <option value="Back-end-program">Back-end Program</option>
+          <option value="Front-end-program">Front-end Program</option>
+        </select> : <p className="s-text-body">{program}</p> }
+        </div>
         <p className="s-text-body">Cohort {user.cohort}</p>
         <section className="contact-info"> 
           <div className="info-box"> 
@@ -28,7 +49,9 @@ const UserProfile = ({ user }) => {
             <p className="contact s-text-body">{user.slack_handle}</p>
           </div>
         </section>
-          <button className="edit-button s-button">Edit Profile </button>
+          <button className="edit-button s-button" onClick={editProfile}>
+            Edit Profile
+          </button>
       </section>
   )
 }
