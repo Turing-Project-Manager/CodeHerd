fdf750c91b87c0f2e9a058b5424c9c8eb6899104
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-08-31 16:28:18 -0600

    Being adding data of projects to the card

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
-
-import PropTypes from 'prop-types';
 import './Project.css'
 
 
-const Project = () => {
+const Project = ({projects, viewingModNumber}) => {
 
+  const projectsByCurrentMod = projects.filter(project => project.mod_number === viewingModNumber).map(currentProject => {
+    return (
+      <div className="project s-card">
+        <div className="s-card-header">
+          <h2 className="s-card-title">{currentProject.name}</h2>
+          <h3 className="s-card-subtitle">Module {currentProject.mod_number}</h3>
+        </div> 
+        <div className="s-card-content">
+          <p>{currentProject.summary}</p>
+        </div>
+        <div className="s-card-footer">
+          Updated {currentProject.updated_at} <br/>
+          Created {currentProject.created_at}
+        </div>
+      </div>
+      
+    )
+  })
 
   return (
-    <>
-      
-    </>
+    <div>
+      {projectsByCurrentMod}
+    </div>
   )
 }
 
--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -1,79 +1,19 @@
-import React from 'react';
-import NewProject from '../NewProject/NewProject';
+import React, { useState } from 'react';
 import './ProjectList.css';
+import Project from '../Project/Project';
 
 
-const ProjectList = ({ projects, user, showProjectForm, closeProjectForm }) => {
+
+const ProjectList = ({ projects }) => {
+  const [viewingModNumber, setViewingModNumber] = useState(1)
 
   const filterByMod = (event) => {
-    const projectsByMod = projects.filter(project => project.mod_number === event.target.value)
-      .map(currentProject => {
-      return (
-        <div class="s-card">
-          <div class="s-card-header">
-            <h2 class="s-card-title">{currentProject.name}</h2>
-            <h3 class="s-card-subtitle">Card Subtitle</h3>
-          </div> 
-          <div class="s-card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum risus odio quis sit mattis in luctus. Dignissim auctor aliquam suspendisse non in ac, purus. Aliquam arcu justo quam lorem at id proin morbi. A arcu accumsan, eu id fringilla non.</p>
-          </div>
-          <div class="s-card-footer">Footer content
-          </div>
-        </div>
-      )
-    })
-    return projectsByMod
+    setViewingModNumber(event.target.value)
   } 
 
-  const projectsByCurrentMod = projects.filter(project => project.mod_number === 4)
-      .map(currentProject => {
-      return (
-        <div class="s-card">
-          <div class="s-card-header">
-            <h2 class="s-card-title">{currentProject.name}</h2>
-            <h3 class="s-card-subtitle">Card Subtitle</h3>
-          </div> 
-          <div class="s-card-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum risus odio quis sit mattis in luctus. Dignissim auctor aliquam suspendisse non in ac, purus. Aliquam arcu justo quam lorem at id proin morbi. A arcu accumsan, eu id fringilla non.</p>
-          </div>
-          <div class="s-card-footer">Footer content
-          </div>
-        </div>
-      )
-    })
-  
-
-
-
-  // const filterByMod = (event) => {
-  //   console.log(projects)
-  //   let projectsByMod = projects.filter((currentProject) => {
-  //     //console.log({currentProject})
-  //     // currentProject.mod_number === event.target.value
-  //   }).map((project, index) => {
-  //     return (
-  //       <div class="s-card">
-  //         <div class="s-card-header">
-  //           <h2 class="s-card-title">Card Title</h2>
-  //           <h3 class="s-card-subtitle">Card Subtitle</h3>
-  //         </div> 
-  //         <div class="s-card-content">
-  //           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum risus odio quis sit mattis in luctus. Dignissim auctor aliquam suspendisse non in ac, purus. Aliquam arcu justo quam lorem at id proin morbi. A arcu accumsan, eu id fringilla non.</p>
-  //         </div>
-  //         <div class="s-card-footer">
-  //           Footer content
-  //         </div>
-  //       </div>
-  //     )
-  //   })
     
-  //   return projectsByMod;
-  // }
-
   return (
     <section className="project-list">
-      {/* <NewProject showProjectForm={showProjectForm} closeProjectForm={closeProjectForm} />
-      <button className='s-button-primary new-proj-btn' onClick={showProjectForm}>New Project</button> */}
       <section className="mod-selection"> 
         <button className="mod-button s-link" value="1" onClick={filterByMod}>Mod 1</button>
         <button className="mod-button s-link" value="2" onClick={filterByMod}>Mod 2</button>
@@ -81,7 +21,7 @@ const ProjectList = ({ projects, user, showProjectForm, closeProjectForm }) => {
         <button className="mod-button s-link" value="4" onClick={filterByMod}>Mod 4</button>
       </section>
       <section className="project-section"> 
-        {projectsByCurrentMod}
+        <Project projects={projects} viewingModNumber={viewingModNumber} />
       </section>
     </section>
   )
