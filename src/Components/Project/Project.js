ef3515c61bb3e2b9b02cb27fb657e519a4d643b5
Ashton Huxtable <aehuxtable@gmail.com>
2021-08-30 20:54:09 -0600

    Remove form information from Project

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,25 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import NewProject from '../NewProject/NewProject';
+
 import PropTypes from 'prop-types';
 import './Project.css'
 
 
 const Project = () => {
 
-  const [showForm, setShowForm] = useState(false)
-
-  const showProjectForm = () => {
-    setShowForm(true)
-  }
-
-  const closeProjectForm = () => {
-    setShowForm(false)
-  }
 
   return (
     <>
-      <button onClick={showProjectForm}>New Project</button>
-      <NewProject showForm={showForm} closeProjectForm={closeProjectForm}/>
+      
     </>
   )
 }
