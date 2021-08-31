b60aa7c8bdc2b5c30bb0acfd04fb7ce36ae72dae
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-08-31 16:56:24 -0600

    Map collaborator names for each project

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Project.css'
 
 
 const Project = ({projects, viewingModNumber}) => {
 
+
+
+
   const projectsByCurrentMod = projects.filter(project => project.mod_number === viewingModNumber).map(currentProject => {
     return (
-      <div className="project s-card">
+      <div className="project s-card" key={currentProject.id}>
         <div className="s-card-header">
-          <h2 className="s-card-title">{currentProject.name}</h2>
-          <h3 className="s-card-subtitle">Module {currentProject.mod_number}</h3>
+          <h2 className="s-card-title tile">{currentProject.name}</h2>
+          <h3 className="s-card-subtitle subtitle">Module {currentProject.mod_number}</h3>
         </div> 
         <div className="s-card-content">
           <p>{currentProject.summary}</p>
+          <div>
+            {currentProject.collaborators.map(collaborator => `${collaborator.name} `)}
+          </div>
         </div>
         <div className="s-card-footer">
           Updated {currentProject.updated_at} <br/>
