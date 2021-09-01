dc740ba6d8d3b2db6196585ec4811aed42c99b34
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-09-01 16:53:19 -0600

    Connect profile picture to Github url property

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -31,7 +31,7 @@ const UserProfile = ({ user }) => {
 
   return (
       <section className="profile-container">
-        <img src={profilePic} className="profile-pic" alt="imported from Github profile"/>
+        <img src={user.url} className="profile-pic" alt="imported from Github profile"/>
         <h2 className="s-h2">{user.name}</h2>
         <p className="s-text-body"> {user.pronouns} </p>
         <div>
