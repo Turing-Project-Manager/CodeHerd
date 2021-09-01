41ab235956968e7f0b88f85e58beaac323cb44e3
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-01 11:12:33 -0600

    Add basic styling to project dashboard

--- src/Components/ProjectDashboard/ProjectDashboard.js
+++ src/Components/ProjectDashboard/ProjectDashboard.js
@@ -3,6 +3,9 @@ import ProjectNav from '../ProjectNav/ProjectNav'
 import ProjectLinks from '../ProjectLinks/ProjectLinks'
 import PropTypes from 'prop-types'
 
+import plus from '../../assets/plus.png'
+import'./ProjectDashboard.css'
+
 
 const ProjectDashboard = ({project}) => {
 
@@ -19,10 +22,14 @@ const ProjectDashboard = ({project}) => {
   return (
     <>
       <ProjectNav />
-      <main>
-        <h2>Project Title</h2>
+      <main className='project-dashboard'>
+        <h2 className='s-h2 s-text-center'>Project Title</h2>
         <ProjectLinks showAddLink={showAddLink} closeLinkForm={closeLinkForm} />
-        <button onClick={showLinkForm}>Add Project Links!</button>
+        <div className='add-link-btn-text'>
+          <button className='s-button-secondary s-border-radius-2 add-link-btn'onClick={showLinkForm}>
+            <img className='plus' src={plus} alt='plus sign' /></button>
+          <h4>Add Links</h4>
+        </div>
         
 
       </main>
