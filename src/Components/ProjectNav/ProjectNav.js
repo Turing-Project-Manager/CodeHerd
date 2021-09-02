fd939dc384537a83cdae0fcb980fc6b6547e3007
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-02 17:42:17 -0600

    Update button sizes and positions

--- src/Components/ProjectNav/ProjectNav.js
+++ src/Components/ProjectNav/ProjectNav.js
@@ -9,9 +9,9 @@ const ProjectNav = () => {
     <aside className='nav-bar-proj'>
       <img className='logo' src={elephant} alt='elephant logo' />
       <section className='project-nav'>
+        <button className='s-button proj-nav-btn coll-btn'>Collaborators</button>
         <button className='s-button proj-nav-btn'>Resources</button>
         <button className='s-button proj-nav-btn'>Templates</button>
-        <button className='s-button proj-nav-btn'>Collaborators</button>
       </section>
       <button className='s-button-primary-inverse'>Logout</button>
     </aside>)
