29efd9ac25a5c9eb8d17bfcd74cfe8340abff98b
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-05 11:24:37 -0600

    Create variables and hooks to hold state

--- src/Components/Templates/Templates.js
+++ src/Components/Templates/Templates.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Markdown from 'markdown-to-jsx'
 
 
 const Templates = () => {
-  
+
+  const fePRtemplate = 'frontend-pr-template.md'
+  const [fePR, setFePR] = useState('')
+
+  return(
+    <section className="templates">
+      <Markdown>
+          {/* {post} */}
+      </Markdown>
+    </section>
+  )
+
 
 }
 
