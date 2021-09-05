9e1b461877a28bcb935cbcac38e2a3e1f86b3bd2
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-05 09:22:11 -0600

    Create basic project resource set up

--- src/Components/ProjectResources/ProjectResources.js
+++ src/Components/ProjectResources/ProjectResources.js
@@ -1,12 +1,57 @@
 import React from 'react'
 
+const initialState = {
+  content: '',
+  name: '',
+  project: {},
+  resourceType: '',
+  tags: []
+}
+
 const ProjectResources = () => {
 
+  const [newResource, setNewResource] = useState(initialState)
+  const [resources, setResources] = useState([])
 
+  const handleResourceInput = (e) => {
+    const { name, value } = e.target;
+    setNewResource((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  const submitResource = (e) => {
+    e.preventDefault();
+    {newResource.content.length && newResource.name.length && newResource.resourceType.length &&
+      setResources(allResources => [...allResources, newResource])
+    }
+    clearInputs();
+  }
+
+  const resourcesToDisplay = resources.map(resource => {
+    return(
+      <a className='proj-resource' 
+        href={resource.content} 
+        key={resource.content}>{resource.name}</a>
+    )
+  })
+
+  const clearInputs = () => {
+    setNewResource({...initialState})
+  }
 
   return(
-    <>
-    </>
+    <section className='project-resources'>
+      <article className='s-h3 s-shadow-md resources-to-display'>
+        <h3 className='s-text-center s-m-3'>Project resources</h3>
+        {!resources.length ?
+          <p className='s-font-lg s-text-center .s-m-3'>No project resources yet! Click below to add one.</p> :
+          <div className='resource-names'>
+            {resourcesToDisplay}
+          </div>
+        }
+      </article>
+      
+      
+    </section>
   )
 
 }
