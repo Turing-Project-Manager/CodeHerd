6f570a9fe42cff22c1d063e2baf36b3bca751c5c
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-07 10:18:58 -0600

    Add tests for finsing infor on main page and GH auth

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -2,19 +2,28 @@ describe('User Landing', () => {
 
 
   beforeEach(() => {
-
+      cy.visit('http://localhost:3000/')
+      Cypress.Cookies.debug(true)
+      Cypress.Cookies.preserveOnce('session_id', 'remember_token')
   })
   
   it('Should have a title and subtitle', () => {
     cy.get('h1').contains('CodeHerd')
-      .get('p[class="app-description]').contains('Search Less, Code More')
+      .get('p').contains('Search Less, Code More')
   })
 
   it('Should show three sections that describe the app', () => {
-    cy.get()
+    cy.get('h2').contains('CREATE A PROJECT')
+      .get('h2').contains('ADD YOUR TEAM')
+      .get('h2').contains('EVERYTHING IN ONE PLACE')
   })
 
   it('Should have a button to continue with GitHub', () => {
     cy.get('button').contains('Continue with Github').click()
-  })
+    .get('input[id="login_field"]').type('aehuxtable@gmail.com')
+    .should('have.value', 'aehuxtable@gmail.com')
+    .get('input[id="password"]').type('password')
+    .should('have.value', 'password')
+    })
+  
 })
