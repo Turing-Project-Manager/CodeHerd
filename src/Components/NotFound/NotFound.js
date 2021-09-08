ffe717e701195910b6a200253f4dda0c6e9a6086
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-07 18:36:20 -0600

    Create basic 404 component if url does not exist

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+
+  return (
+    <section className='not-found'>
+      <h3>Page could not be found, please try again later.</h3>
+    </section>
+  )
+}
+
+export default NotFound
