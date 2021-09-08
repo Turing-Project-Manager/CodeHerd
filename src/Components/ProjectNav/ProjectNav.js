29de340707ef52b87ae0c1998b8a2dfc15c4ce75
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-07 19:25:52 -0600

    Update project dashboard buttons for navigation

--- src/Components/ProjectNav/ProjectNav.js
+++ src/Components/ProjectNav/ProjectNav.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import elephant from '../../assets/elephant.png'
 import '../Nav/Nav.css'
 
-const ProjectNav = () => {
+const ProjectNav = ({githubHandle}) => {
 
   return(
     <aside className='nav-bar-proj'>
-      <img className='logo' src={elephant} alt='elephant logo' />
+      <Link className='logo' to={`/${githubHandle}`} >
+        <img className='logo' src={elephant} alt='elephant logo' />
+      </Link >
       <section className='project-nav'>
         <button className='s-button proj-nav-btn coll-btn'>Collaborators</button>
         <button className='s-button proj-nav-btn'>Resources</button>
         <button className='s-button proj-nav-btn'>Templates</button>
       </section>
-      <button className='s-button-primary-inverse'>Logout</button>
+      <Link to='/'>
+        <button className='s-button-primary-inverse'>Logout</button>
+      </Link>
     </aside>)
 
 }
